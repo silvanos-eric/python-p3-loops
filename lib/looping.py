08bf09827ecba2b837dd1ca3fb54129f99c71baa
Silvanos Eric <ericoderito@gmail.com>
2024-09-01 08:23:44 +0300

    feat: add Python 3.8.13 requirement and implement looping functions
    - Added `.mise.toml` file specifying Python 3.8.13 as the required version.
    - Implemented `happy_new_year` function to countdown from 10 and print "Happy New Year!".
    - Implemented `square_integers` function to return a list of squared integers.
    - Implemented `fizzbuzz` function to print numbers from 1 to 100 with FizzBuzz logic.

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while True:
+        if counter == 0:
+            print('Happy New Year!')
+            break
+        print(counter)
+        counter -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 5 == 0 and num % 3 == 0:
+            print('FizzBuzz')
+        elif num % 5 == 0:
+            print('Buzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        else:
+            print(num)
